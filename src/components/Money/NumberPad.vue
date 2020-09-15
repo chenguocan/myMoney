02f2c3cc205fc7b1ecb48b3e3c310a928cad8fb9
chenguocan <2622870670@qq.com>
2020-09-15 18:07:44 +0800

    基本需求

--- src/components/Money/NumberPad.vue
+++ src/components/Money/NumberPad.vue
@@ -29,7 +29,7 @@ export default class NumberPad extends Vue {
   output = '0';
   inputContent(event: MouseEvent) {
     const btn = (event.target as HTMLButtonElement);
-    const input = btn.textContent!;
+    const input= btn.textContent!;
     if (this.output.length === 16) {
       return;
     }
@@ -58,8 +58,9 @@ export default class NumberPad extends Vue {
     this.output="0";
   }
   ok(){
-    this.$emit("update:Amount",this.output);
-    this.$emit("submit",this.output);
+    const number=parseFloat(this.output)
+    this.$emit("update:Amount",number);
+    this.$emit("submit",number);
     this.output="0";
   }
 }
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -2,10 +2,9 @@
   <div>
     <Layout>
       <Tabs  :class-prefix="'type'" :date-source="recordType" :type.sync="type"></Tabs>
-      <Tabs  :class-prefix="'interval'" :date-source="intervalList" class="day" :type.sync="dayType"></Tabs>
       <ol>
-        <li v-for="(group,index) in result" :key="index">
-          <h3 class="title">{{group.title}}</h3>
+        <li v-for="(group,index) in groupList" :key="index">
+          <h3 class="title">{{beautify(group.title)}} <span>{{group.total}}</span></h3>
           <ol>
             <li class="record" v-for="item in group.items" :key="item.id">
              <span>{{toStringTags(item.tags)}}</span> <span class="note">{{item.notes}}</span><span>￥{{item.amount}}</span>
@@ -22,40 +21,59 @@ import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
 import recordList from '@/constant/recordList';
-import intervalList from '@/constant/intervalList';
-
+import dayjs from 'dayjs'
+import clone from "@/lib/clone.ts"
 @Component({
   components: {Tabs}
 })
 export default class Statistics extends Vue {
+  beautify(string: string) {
+    const day = dayjs(string);
+    const now = dayjs();
+    if (day.isSame(now, 'day')) {
+      return '今天';
+    } else if (day.isSame(now.subtract(1, 'day'), 'day')) {
+      return '昨天';
+    } else if (day.isSame(now.subtract(2, 'day'), 'day')) {
+      return '前天';
+    } else if (day.isSame(now, 'year')) {
+      return day.format('M月D日');
+    } else {
+      return day.format('YYYY年M月D日');
+    }
+  }
   toStringTags(tags: Tag[]){
-    console.log(tags);
     return tags.length === 0 ? '无' :tags.join(',');
   }
   get recordList(){
     return this.$store.state.recordList;
   }
 
-  get result(){
+  get groupList(){
     const {recordList}=this;
-    type HashTableValue={ title: string;items: RecordItem[]};
-    const hashTable: {[key: string]: HashTableValue}={};
-    for(let i=0;i<recordList.length;i++){
-      const date=recordList[i].createdAt.split("T")[0];
-      const time=recordList[i].createdAt.split("T")[1];
-      hashTable[date]=hashTable[date]||{title:date,items:[]};
-      hashTable[date].items.push(recordList[i]);
-      console.log(hashTable);
+    const newList=clone(recordList).filter((r: RecordItem)=>r.type===this.type).sort((a: any, b: any) => dayjs(b.createdAt).valueOf() - dayjs(a.createdAt).valueOf());
+    type Result={title: string;total?: number;items: RecordItem[]}[]
+    const result: Result=[{title:dayjs(newList[0].createdAt).format("YYYY-MM-DD"),items:[newList[0]]}]
+    for(let i=1;i<newList.length;i++){
+      const current=newList[i];
+      const last=result[result.length-1];
+      if(dayjs(last.title).isSame(current.createdAt),'day'){
+          last.items.push(current);
+      }else{
+        result.push({title:dayjs(current.createdAt).format('YYYY-MM-DD'),items:[current]})
+      }
     }
-    return hashTable;
+    result.map(group=>group.total=group.items.reduce((sum,item)=>{
+          return sum+item.amount;
+    },0)
+    )
+    return result;
   }
   type="-";
   dayType="day"
   recordType=recordList;
-  intervalList=intervalList;
 
   created(){
-    console.log(this.result)
    this.$store.commit("fetchRecords");
   }
 }
